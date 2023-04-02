7c11d1311aa3d4ad5df7d5a74712098aa0dae295
Mvideo <Mvideo@LAPTOP-C15K7SNF>
2023-04-02 21:39:08 +0300

    Добавьте файлы проекта.

---
+++ Pages/PagePW6.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfAppPractwork1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PagePW6.xaml
+    /// </summary>
+    public partial class PagePW6 : Page
+    {
+        public PagePW6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"Строки.txt", Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                lstInput.Items.Add(sr.ReadLine());
+            }
+            sr.Close();
+        }
+
+        private void btnRezult_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int index = lstInput.SelectedIndex;
+                string str = (string)lstInput.Items[index];
+                int len = str.Length;
+                char[] rusalf = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+                int i = 0;
+                int count = 0;
+                while (i < len - 1)
+                {
+                    foreach(char c in str)
+                        if (c == rusalf[i])
+                        count++;
+                    i++;
+                }
+                txbRezult.Text = "Количество строчных русских букв = " + count.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Pages/PagePW7.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppPractwork1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PagePW7.xaml
+    /// </summary>
+    public partial class PagePW7 : Page
+    {
+        int[] Y = new int[25];
+        public PagePW7()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Создание массива
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCreateArray_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            for(int i = 0; i < Y.Length; i++) 
+            {
+                Y[i] = rand.Next(-1000,1000);
+                LstArray.Items.Add($"#{i}  {Y[i]}");
+            }
+        }
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSolveArray_Click(object sender, RoutedEventArgs e)
+        {
+
+            double[] r = new double[25];
+            for (int i = 0; i < Y.Length; i++)
+            {
+                r[i] = (5 * Y[i] + Math.Pow(Math.Cos(Y[i]), 2)) / 2.35;
+                LstArrayNew.Items.Add($"#{i}  {r[i]}");
+            }
+        }
+    }
+}
